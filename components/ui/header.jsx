35b9f259870b8c929e64ee1e77f3eba81da8d6ef
kushagra singhal <kushagrasinghal987@gmail.com>
2025-07-01 00:20:02 +0530

    Intial project Setup

---
+++ components/ui/header.jsx
@@ -0,0 +1,19 @@
+"use client";
+import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
+import React from 'react'
+
+const Header = () => {
+return (
+  <div>
+    <SignedOut>
+      <SignInButton/>
+      <SignUpButton/>
+    </SignedOut>
+ <SignedIn>
+  <UserButton/>
+ </SignedIn>
+  </div>
+);
+};
+
+export default Header;
