416225e5d95cb235415b92008ef4c4ceb2f841e1
kushagra singhal <kushagrasinghal987@gmail.com>
2025-07-02 13:50:14 +0530

    Made heade component

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-  <div>hello</div>
+  <div className="pt-16">hello
+    <br />
+    <Button variant={"destructive"}>subscribe</Button>
+  </div>
   );
 }
